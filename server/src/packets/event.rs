b1c425993aa9dc157a1a21756bc824d8d3741339
Phill <philltv.gaming@gmail.com>
2024-01-23 00:36:03 +0100

    feat: add GameEvent packet

---
+++ server/src/packets/event.rs
@@ -0,0 +1,16 @@
+use crate::encoder::Encoder;
+use macros::Streamable;
+use std::io::Write;
+
+#[derive(Streamable)]
+#[packet_id(0x20)]
+pub struct GameEvent {
+    event: u8,
+    value: f32,
+}
+
+impl Default for GameEvent {
+    fn default() -> Self {
+        Self { event: 13, value: 0.0 }
+    }
+}
