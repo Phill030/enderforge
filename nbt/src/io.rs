6ef623dbedd00b5cb1f41d0764b3e15d4fdd70b6
Phill <philltv.gaming@gmail.com>
2024-02-15 13:01:31 +0100

    ref(nbt): convert functions to async

--- nbt/src/io.rs
+++ nbt/src/io.rs
@@ -1,6 +1,6 @@
 #![allow(private_bounds)]
 use super::types::{NbtReader, NbtWriter, Tag};
-use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
+use async_recursion::async_recursion;
 use flate2::{
     read::{GzDecoder, ZlibDecoder},
     write::{GzEncoder, ZlibEncoder},
@@ -9,8 +9,9 @@ use flate2::{
 use std::{
     collections::HashMap,
     fmt,
-    io::{self, Read, Result, Write},
+    io::{self, Result},
 };
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 
 #[derive(Clone, Debug, Default, PartialEq)]
 pub struct Nbt {
@@ -28,63 +29,72 @@ impl Nbt {
         }
     }
 
-    pub fn to_writer<W: Write + NbtWriter>(&self, writer: &mut W) -> io::Result<()> {
-        writer.write_u8(0x0a)?;
-        writer.write_bare_string(&self.title)?;
+    pub async fn to_writer<W>(&self, writer: &mut W) -> io::Result<()>
+    where
+        W: AsyncWrite + NbtWriter + Unpin + Send,
+    {
+        writer.write_u8(0x0a).await?;
+        writer.write_bare_string(&self.title).await?;
 
         for (name, nbt) in self.content.iter() {
-            writer.write_u8(nbt.id())?;
-            writer.write_bare_string(name)?;
-            nbt.encode(writer)?;
+            writer.write_u8(nbt.id()).await?;
+            writer.write_bare_string(name).await?;
+            nbt.encode(writer).await?;
         }
 
-        Ok(writer.close_nbt()?)
+        Ok(writer.close_nbt().await?)
     }
 
     /// Networked NBT's are missing the title of the root `TAG_COMPOUND`
-    pub fn to_networked_writer<W: Write + NbtWriter>(&self, writer: &mut W) -> io::Result<()> {
-        writer.write_u8(0x0a)?;
+    pub async fn to_networked_writer<W>(&self, writer: &mut W) -> io::Result<()>
+    where
+        W: AsyncWrite + NbtWriter + Unpin + Send,
+    {
+        writer.write_u8(0x0a).await?;
 
         for (name, nbt) in self.content.iter() {
-            writer.write_u8(nbt.id())?;
-            writer.write_bare_string(name)?;
-            nbt.encode(writer)?;
+            writer.write_u8(nbt.id()).await?;
+            writer.write_bare_string(name).await?;
+            nbt.encode(writer).await?;
         }
 
-        Ok(writer.close_nbt()?)
+        Ok(writer.close_nbt().await?)
     }
 
-    pub fn to_zlib_writer<W: Write + NbtWriter>(&mut self, writer: &mut W) -> io::Result<()> {
-        Ok(self.to_writer(&mut GzEncoder::new(writer, Compression::default()))?)
-    }
+    // pub async fn to_zlib_writer<W: AsyncWrite + NbtWriter>(&mut self, writer: &mut W) -> io::Result<()> {
+    //     Ok(self.to_writer(&mut GzEncoder::new(writer, Compression::default())).await?)
+    // }
 
-    pub fn to_gzip_writer<W: Write + NbtWriter>(&self, writer: &mut W) -> io::Result<()> {
-        Ok(self.to_writer(&mut ZlibEncoder::new(writer, Compression::default()))?)
-    }
+    // pub async fn to_gzip_writer<W: AsyncWrite + NbtWriter>(&self, writer: &mut W) -> io::Result<()> {
+    //     Ok(self.to_writer(&mut ZlibEncoder::new(writer, Compression::default())).await?)
+    // }
 
-    pub fn from_reader<R: Read + NbtReader>(reader: &mut R) -> io::Result<Nbt> {
-        let (tag, title) = reader.emit_next_header()?;
+    pub async fn from_reader<R>(reader: &mut R) -> io::Result<Nbt>
+    where
+        R: AsyncRead + NbtReader + Unpin + Send,
+    {
+        let (tag, title) = reader.emit_next_header().await?;
 
         if tag != 0x0a {
             panic!("TODO HANDLE ERROR! Must start with Root compound");
         }
 
-        let content = Tag::decode(tag, reader);
+        let content = Tag::decode(tag, reader).await;
         match content {
             Ok(Tag::Compound(map)) => Ok(Nbt { title, content: map }),
             _ => panic!("TODO HANDLE ERROR! NO ROOT COMPONENT"),
         }
     }
 
-    pub fn from_zlib_reader<R: Read + NbtReader>(reader: &mut R) -> io::Result<Nbt> {
-        let mut zlib = ZlibDecoder::new(reader);
-        Self::from_reader(&mut zlib)
-    }
+    // pub async fn from_zlib_reader<R: AsyncRead + NbtReader>(reader: &mut R) -> io::Result<Nbt> {
+    //     let mut zlib = ZlibDecoder::new(reader);
+    //     Self::from_reader(&mut zlib)
+    // }
 
-    pub fn from_gzip_reader<R: Read + NbtReader>(reader: &mut R) -> io::Result<Nbt> {
-        let mut zlib = GzDecoder::new(reader);
-        Self::from_reader(&mut zlib)
-    }
+    // pub async fn from_gzip_reader<R: AsyncRead + NbtReader>(reader: &mut R) -> io::Result<Nbt> {
+    //     let mut zlib = GzDecoder::new(reader);
+    //     Self::from_reader(&mut zlib)
+    // }
 }
 
 impl fmt::Display for Nbt {
@@ -134,84 +144,86 @@ impl Tag {
         }
     }
 
-    fn encode<W: Write>(&self, writer: &mut W) -> io::Result<()> {
+    #[async_recursion()]
+    async fn encode<W: AsyncWrite + Unpin + Send>(&self, writer: &mut W) -> io::Result<()> {
         Ok(match self {
-            Tag::Byte(value) => writer.write_bare_byte(*value)?,
-            Tag::Short(value) => writer.write_bare_short(*value)?,
-            Tag::Int(value) => writer.write_bare_int(*value)?,
-            Tag::Long(value) => writer.write_bare_long(*value)?,
-            Tag::Float(value) => writer.write_bare_float(*value)?,
-            Tag::Double(value) => writer.write_bare_double(*value)?,
-            Tag::String(value) => writer.write_bare_string(value)?,
+            Tag::Byte(value) => writer.write_bare_byte(*value).await?,
+            Tag::Short(value) => writer.write_bare_short(*value).await?,
+            Tag::Int(value) => writer.write_bare_int(*value).await?,
+            Tag::Long(value) => writer.write_bare_long(*value).await?,
+            Tag::Float(value) => writer.write_bare_float(*value).await?,
+            Tag::Double(value) => writer.write_bare_double(*value).await?,
+            Tag::String(value) => writer.write_bare_string(value).await?,
             Tag::List(values) => {
                 if values.is_empty() {
-                    writer.write_u8(0)?; // TAG_End
-                    writer.write_i32::<BigEndian>(0)?; // Length of the list
+                    writer.write_u8(0).await?; // TAG_End
+                    writer.write_i32(0).await?; // Length of the list
                 } else {
                     let first_id = values[0].id();
 
-                    writer.write_u8(first_id)?;
-                    writer.write_i32::<BigEndian>(values.len() as i32)?;
+                    writer.write_u8(first_id).await?;
+                    writer.write_i32(values.len() as i32).await?;
 
                     for nbt in values {
                         if nbt.id() != first_id {
                             todo!("HANDLE PANIC!");
                         }
 
-                        nbt.encode(writer)?;
+                        nbt.encode(writer).await?;
                     }
                 }
             }
             Tag::Compound(values) => {
                 for (name, nbt) in values {
-                    writer.write_u8(nbt.id())?;
-                    writer.write_bare_string(name)?;
-                    nbt.encode(writer)?;
+                    writer.write_u8(nbt.id()).await?;
+                    writer.write_bare_string(name).await?;
+                    nbt.encode(writer).await?;
                 }
 
-                writer.close_nbt()?;
+                writer.close_nbt().await?;
             }
-            Tag::ByteArray(values) => writer.write_bare_byte_array(&values[..])?,
-            Tag::IntArray(values) => writer.write_bare_int_array(&values[..])?,
-            Tag::LongArray(values) => writer.write_bare_long_array(&values[..])?,
+            Tag::ByteArray(values) => writer.write_bare_byte_array(&values[..]).await?,
+            Tag::IntArray(values) => writer.write_bare_int_array(&values[..]).await?,
+            Tag::LongArray(values) => writer.write_bare_long_array(&values[..]).await?,
         })
     }
 
-    fn decode<R: Read>(id: u8, reader: &mut R) -> Result<Tag> {
+    #[async_recursion()]
+    async fn decode<R: AsyncRead + Unpin + Send>(id: u8, reader: &mut R) -> Result<Tag> {
         Ok(match id {
-            0x01 => Tag::Byte(reader.read_bare_byte()?),
-            0x02 => Tag::Short(reader.read_bare_short()?),
-            0x03 => Tag::Int(reader.read_bare_int()?),
-            0x04 => Tag::Long(reader.read_bare_long()?),
-            0x05 => Tag::Float(reader.read_bare_float()?),
-            0x06 => Tag::Double(reader.read_bare_double()?),
-            0x07 => Tag::ByteArray(reader.read_bare_byte_array()?),
-            0x08 => Tag::String(reader.read_bare_string()?),
+            0x01 => Tag::Byte(reader.read_bare_byte().await?),
+            0x02 => Tag::Short(reader.read_bare_short().await?),
+            0x03 => Tag::Int(reader.read_bare_int().await?),
+            0x04 => Tag::Long(reader.read_bare_long().await?),
+            0x05 => Tag::Float(reader.read_bare_float().await?),
+            0x06 => Tag::Double(reader.read_bare_double().await?),
+            0x07 => Tag::ByteArray(reader.read_bare_byte_array().await?),
+            0x08 => Tag::String(reader.read_bare_string().await?),
             0x09 => {
-                let id = reader.read_u8()?;
-                let len = reader.read_i32::<BigEndian>()? as usize;
+                let id = reader.read_u8().await?;
+                let len = reader.read_i32().await? as usize;
                 let mut buf = Vec::with_capacity(len);
                 for _ in 0..len {
-                    buf.push(Tag::decode(id, reader)?);
+                    buf.push(Tag::decode(id, reader).await?);
                 }
                 Tag::List(buf)
             }
             0x0a => {
                 let mut map = HashMap::new();
                 loop {
-                    let (id, name) = reader.emit_next_header()?;
+                    let (id, name) = reader.emit_next_header().await?;
 
                     if id.eq(&0x00) {
                         break;
                     }
 
-                    let tag = Tag::decode(id, reader)?;
+                    let tag = Tag::decode(id, reader).await?;
                     map.insert(name, tag);
                 }
                 Tag::Compound(map)
             }
-            0x0b => Tag::IntArray(reader.read_bare_int_array()?),
-            0x0c => Tag::LongArray(reader.read_bare_long_array()?),
+            0x0b => Tag::IntArray(reader.read_bare_int_array().await?),
+            0x0c => Tag::LongArray(reader.read_bare_long_array().await?),
             _ => panic!("TODO HANDLE ERROR! INVALID TYPE ID "),
         })
     }
--- nbt/src/types.rs
+++ nbt/src/types.rs
@@ -1,8 +1,5 @@
-use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
-use std::{
-    collections::HashMap,
-    io::{Read, Result, Write},
-};
+use std::{collections::HashMap, io::Result};
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Tag {
@@ -19,194 +16,195 @@ pub enum Tag {
     IntArray(Vec<i32>),
     LongArray(Vec<i64>),
 }
+
 pub(super) trait NbtWriter {
-    fn close_nbt(&mut self) -> Result<()>;
-    fn write_bare_string(&mut self, value: &str) -> Result<()>;
-    fn write_bare_byte(&mut self, value: i8) -> Result<()>;
-    fn write_bare_short(&mut self, value: i16) -> Result<()>;
-    fn write_bare_int(&mut self, value: i32) -> Result<()>;
-    fn write_bare_long(&mut self, value: i64) -> Result<()>;
-    fn write_bare_float(&mut self, value: f32) -> Result<()>;
-    fn write_bare_double(&mut self, value: f64) -> Result<()>;
-    fn write_bare_int_array(&mut self, value: &[i32]) -> Result<()>;
-    fn write_bare_long_array(&mut self, value: &[i64]) -> Result<()>;
-    fn write_bare_byte_array(&mut self, value: &[i8]) -> Result<()>;
+    async fn close_nbt(&mut self) -> Result<()>;
+    async fn write_bare_string(&mut self, value: &str) -> Result<()>;
+    async fn write_bare_byte(&mut self, value: i8) -> Result<()>;
+    async fn write_bare_short(&mut self, value: i16) -> Result<()>;
+    async fn write_bare_int(&mut self, value: i32) -> Result<()>;
+    async fn write_bare_long(&mut self, value: i64) -> Result<()>;
+    async fn write_bare_float(&mut self, value: f32) -> Result<()>;
+    async fn write_bare_double(&mut self, value: f64) -> Result<()>;
+    async fn write_bare_int_array(&mut self, value: &[i32]) -> Result<()>;
+    async fn write_bare_long_array(&mut self, value: &[i64]) -> Result<()>;
+    async fn write_bare_byte_array(&mut self, value: &[i8]) -> Result<()>;
 }
 
-impl<W: Write> NbtWriter for W {
+impl<W: AsyncWrite + Unpin> NbtWriter for W {
     #[inline]
-    fn close_nbt(&mut self) -> Result<()> {
-        Ok(self.write_u8(0x00)?)
+    async fn close_nbt(&mut self) -> Result<()> {
+        Ok(self.write_u8(0x00).await?)
     }
 
     #[inline]
-    fn write_bare_string(&mut self, value: &str) -> Result<()> {
+    async fn write_bare_string(&mut self, value: &str) -> Result<()> {
         let mod_utf8 = cesu8::to_java_cesu8(value);
-        self.write_u16::<BigEndian>(mod_utf8.len() as u16)?;
-        Ok(self.write_all(&mod_utf8)?)
+        self.write_u16(mod_utf8.len() as u16).await?;
+        Ok(self.write_all(&mod_utf8).await?)
     }
 
     #[inline]
-    fn write_bare_byte(&mut self, value: i8) -> Result<()> {
-        Ok(self.write_i8(value)?)
+    async fn write_bare_byte(&mut self, value: i8) -> Result<()> {
+        Ok(self.write_i8(value).await?)
     }
 
     #[inline]
-    fn write_bare_short(&mut self, value: i16) -> Result<()> {
-        Ok(self.write_i16::<BigEndian>(value)?)
+    async fn write_bare_short(&mut self, value: i16) -> Result<()> {
+        Ok(self.write_i16(value).await?)
     }
 
     #[inline]
-    fn write_bare_int(&mut self, value: i32) -> Result<()> {
-        Ok(self.write_i32::<BigEndian>(value)?)
+    async fn write_bare_int(&mut self, value: i32) -> Result<()> {
+        Ok(self.write_i32(value).await?)
     }
 
     #[inline]
-    fn write_bare_long(&mut self, value: i64) -> Result<()> {
-        Ok(self.write_i64::<BigEndian>(value)?)
+    async fn write_bare_long(&mut self, value: i64) -> Result<()> {
+        Ok(self.write_i64(value).await?)
     }
 
     #[inline]
-    fn write_bare_float(&mut self, value: f32) -> Result<()> {
-        Ok(self.write_f32::<BigEndian>(value)?)
+    async fn write_bare_float(&mut self, value: f32) -> Result<()> {
+        Ok(self.write_f32(value).await?)
     }
 
     #[inline]
-    fn write_bare_double(&mut self, value: f64) -> Result<()> {
-        Ok(self.write_f64::<BigEndian>(value)?)
+    async fn write_bare_double(&mut self, value: f64) -> Result<()> {
+        Ok(self.write_f64(value).await?)
     }
 
     #[inline]
-    fn write_bare_int_array(&mut self, value: &[i32]) -> Result<()> {
-        self.write_i32::<BigEndian>(value.len() as i32)?;
+    async fn write_bare_int_array(&mut self, value: &[i32]) -> Result<()> {
+        self.write_i32(value.len() as i32).await?;
         for &v in value {
-            self.write_i32::<BigEndian>(v)?;
+            self.write_i32(v).await?;
         }
         Ok(())
     }
 
     #[inline]
-    fn write_bare_long_array(&mut self, value: &[i64]) -> Result<()> {
-        self.write_i32::<BigEndian>(value.len() as i32)?;
+    async fn write_bare_long_array(&mut self, value: &[i64]) -> Result<()> {
+        self.write_i32(value.len() as i32).await?;
         for &v in value {
-            self.write_i64::<BigEndian>(v)?;
+            self.write_i64(v).await?;
         }
         Ok(())
     }
 
     #[inline]
-    fn write_bare_byte_array(&mut self, value: &[i8]) -> Result<()> {
-        self.write_i32::<BigEndian>(value.len() as i32)?;
+    async fn write_bare_byte_array(&mut self, value: &[i8]) -> Result<()> {
+        self.write_i32(value.len() as i32).await?;
         for &v in value {
-            self.write_i8(v)?;
+            self.write_i8(v).await?;
         }
         Ok(())
     }
 }
 
 pub(super) trait NbtReader {
-    fn emit_next_header(&mut self) -> Result<(u8, String)>;
-    fn read_bare_string(&mut self) -> Result<String>;
-    fn read_bare_byte(&mut self) -> Result<i8>;
-    fn read_bare_short(&mut self) -> Result<i16>;
-    fn read_bare_int(&mut self) -> Result<i32>;
-    fn read_bare_long(&mut self) -> Result<i64>;
-    fn read_bare_float(&mut self) -> Result<f32>;
-    fn read_bare_double(&mut self) -> Result<f64>;
-    fn read_bare_int_array(&mut self) -> Result<Vec<i32>>;
-    fn read_bare_long_array(&mut self) -> Result<Vec<i64>>;
-    fn read_bare_byte_array(&mut self) -> Result<Vec<i8>>;
+    async fn emit_next_header(&mut self) -> Result<(u8, String)>;
+    async fn read_bare_string(&mut self) -> Result<String>;
+    async fn read_bare_byte(&mut self) -> Result<i8>;
+    async fn read_bare_short(&mut self) -> Result<i16>;
+    async fn read_bare_int(&mut self) -> Result<i32>;
+    async fn read_bare_long(&mut self) -> Result<i64>;
+    async fn read_bare_float(&mut self) -> Result<f32>;
+    async fn read_bare_double(&mut self) -> Result<f64>;
+    async fn read_bare_int_array(&mut self) -> Result<Vec<i32>>;
+    async fn read_bare_long_array(&mut self) -> Result<Vec<i64>>;
+    async fn read_bare_byte_array(&mut self) -> Result<Vec<i8>>;
 }
 
-impl<R: Read> NbtReader for R {
-    fn emit_next_header(&mut self) -> Result<(u8, String)> {
-        let tag = self.read_u8()?;
+impl<R: AsyncRead + Unpin> NbtReader for R {
+    async fn emit_next_header(&mut self) -> Result<(u8, String)> {
+        let tag = self.read_u8().await?;
 
         match tag {
             0x00 => Ok((tag, "".to_string())),
             _ => {
-                let name = self.read_bare_string()?;
+                let name = self.read_bare_string().await?;
                 Ok((tag, name))
             }
         }
     }
 
     #[inline]
-    fn read_bare_byte(&mut self) -> Result<i8> {
-        Ok(self.read_i8()?)
+    async fn read_bare_byte(&mut self) -> Result<i8> {
+        Ok(self.read_i8().await?)
     }
 
     #[inline]
-    fn read_bare_short(&mut self) -> Result<i16> {
-        Ok(self.read_i16::<BigEndian>()?)
+    async fn read_bare_short(&mut self) -> Result<i16> {
+        Ok(self.read_i16().await?)
     }
 
     #[inline]
-    fn read_bare_int(&mut self) -> Result<i32> {
-        Ok(self.read_i32::<BigEndian>()?)
+    async fn read_bare_int(&mut self) -> Result<i32> {
+        Ok(self.read_i32().await?)
     }
 
     #[inline]
-    fn read_bare_long(&mut self) -> Result<i64> {
-        Ok(self.read_i64::<BigEndian>()?)
+    async fn read_bare_long(&mut self) -> Result<i64> {
+        Ok(self.read_i64().await?)
     }
 
     #[inline]
-    fn read_bare_float(&mut self) -> Result<f32> {
-        Ok(self.read_f32::<BigEndian>()?)
+    async fn read_bare_float(&mut self) -> Result<f32> {
+        Ok(self.read_f32().await?)
     }
 
     #[inline]
-    fn read_bare_double(&mut self) -> Result<f64> {
-        Ok(self.read_f64::<BigEndian>()?)
+    async fn read_bare_double(&mut self) -> Result<f64> {
+        Ok(self.read_f64().await?)
     }
 
     #[inline]
-    fn read_bare_int_array(&mut self) -> Result<Vec<i32>> {
-        let len = self.read_i32::<BigEndian>()? as usize;
+    async fn read_bare_int_array(&mut self) -> Result<Vec<i32>> {
+        let len = self.read_i32().await? as usize;
         let mut buf = Vec::with_capacity(len);
 
         for _ in 0..len {
-            buf.push(self.read_i32::<BigEndian>()?);
+            buf.push(self.read_i32().await?);
         }
 
         Ok(buf)
     }
 
     #[inline]
-    fn read_bare_long_array(&mut self) -> Result<Vec<i64>> {
-        let len = self.read_i32::<BigEndian>()? as usize;
+    async fn read_bare_long_array(&mut self) -> Result<Vec<i64>> {
+        let len = self.read_i32().await? as usize;
 
         let mut buf = Vec::with_capacity(len);
         for _ in 0..len {
-            buf.push(self.read_i64::<BigEndian>()?);
+            buf.push(self.read_i64().await?);
         }
 
         Ok(buf)
     }
 
     #[inline]
-    fn read_bare_byte_array(&mut self) -> Result<Vec<i8>> {
-        let len = self.read_i32::<BigEndian>()? as usize;
+    async fn read_bare_byte_array(&mut self) -> Result<Vec<i8>> {
+        let len = self.read_i32().await? as usize;
         let mut buf = Vec::with_capacity(len);
 
         for _ in 0..len {
-            buf.push(self.read_i8()?);
+            buf.push(self.read_i8().await?);
         }
 
         Ok(buf)
     }
 
     #[inline]
-    fn read_bare_string(&mut self) -> Result<String> {
-        let len = self.read_u16::<BigEndian>()? as usize;
+    async fn read_bare_string(&mut self) -> Result<String> {
+        let len = self.read_u16().await? as usize;
 
         if len == 0 {
             return Ok("".to_string());
         }
 
         let mut bytes = vec![0; len];
-        self.read_exact(&mut bytes)?;
+        self.read_exact(&mut bytes).await?;
 
         let java_decoded = match cesu8::from_java_cesu8(&bytes) {
             Ok(string) => string,
